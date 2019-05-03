15f3eab1f271f82ac41677300123f0661d62d578
M-D-Elliott <40653200+M-D-Elliott@users.noreply.github.com>
2019-05-03 15:03:00 -0500

    Add project files.

---
+++ SmashPopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmashPopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
